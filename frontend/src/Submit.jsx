10da10fd20605cba5301cc698c98c7f40006fb1e
AMAN7040 <amansingh79620@gmail.com>
2025-06-13 20:09:56 +0530

    chore: pre-provided files code managed

---
+++ frontend/src/Submit.jsx
@@ -0,0 +1,15 @@
+// Submit.jsx
+
+export const SubmitButton = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <button type="submit">Submit</button>
+    </div>
+  );
+};
