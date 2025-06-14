648e455b2534138e7e2784251c657770551e19a0
AMAN7040 <amansingh79620@gmail.com>
2025-06-14 23:49:28 +0530

    feat: implement node factory utility to standardize custom node creation

---
+++ frontend/src/utils/createNode.jsx
@@ -0,0 +1,41 @@
+// src/utils/createNode.jsx
+
+import { useState, useCallback } from "react";
+import PropTypes from "prop-types";
+import BaseNode from "../components/BaseNode";
+
+const createNode = ({ type, fields = [], inputs = [], outputs = [] }) => {
+  const Node = ({ id, data }) => {
+    const hydratedData = fields.reduce((acc, field) => {
+      acc[field.name] = data?.[field.name] ?? field.default ?? "";
+      return acc;
+    }, {});
+
+    const [nodeData, setNodeData] = useState(hydratedData);
+
+    const handleChange = useCallback((key, value) => {
+      setNodeData((prev) => ({ ...prev, [key]: value }));
+    }, []);
+
+    return (
+      <BaseNode
+        id={id}
+        title={type}
+        inputs={inputs.map((i) => ({ id: `${id}-${i}` }))}
+        outputs={outputs.map((o) => ({ id: `${id}-${o}` }))}
+        fields={fields}
+        onChange={handleChange}
+        data={nodeData}
+      />
+    );
+  };
+
+  Node.propTypes = {
+    id: PropTypes.string.isRequired,
+    data: PropTypes.object,
+  };
+
+  return Node;
+};
+
+export default createNode;
