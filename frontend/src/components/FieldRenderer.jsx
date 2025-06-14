5bee14ae96142d006729ca7edea268b2c2d7de41
AMAN7040 <amansingh79620@gmail.com>
2025-06-14 22:46:43 +0530

    feat: add dynamic FieldRenderer to support declarative field UI generation

--- frontend/src/components/FieldRenderer.jsx
+++ frontend/src/components/FieldRenderer.jsx
@@ -1,26 +1,55 @@
 // src/components/FieldRenderer.jsx
 
-import React from "react";
+/**
+ * FieldRenderer.jsx
+ *
+ * A dynamic field rendering utility that maps field definitions to actual React field components.
+ * Supports pluggable architecture via COMPONENT_MAP and ensures declarative rendering of form fields.
+ */
 
-const FIELD_MAP = {
-  
+import PropTypes from "prop-types";
+import TextField from "./fields/TextField";
+import DropdownField from "./fields/DropdownField";
+
+const COMPONENT_MAP = {
+  text: TextField,
+  dropdown: DropdownField,
 };
 
-const FieldRenderer = (fields = ["Text", "input", "Text"]) => {
+const FieldRenderer = ({ fields, data, onChange }) => {
   return (
-    <div>
+    <div className="space-y-3">
       {fields.map((field) => {
-        const Component = FIELD_MAP(field.type);
-
+        const Component = COMPONENT_MAP[field.type];
         if (!Component) {
-          console.warn(`Unsupported field type : ${field.type}`);
+          console.warn(`Unsupported field type: ${field.type}`);
           return null;
         }
 
-        return <Component />;
+        return (
+          <Component
+            key={field.name}
+            label={field.label}
+            value={data?.[field.name] ?? field.default ?? ""}
+            onChange={(val) => onChange(field.name, val)}
+            {...field}
+          />
+        );
       })}
     </div>
   );
+};
+
+FieldRenderer.propTypes = {
+  fields: PropTypes.arrayOf(
+    PropTypes.shape({
+      type: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      label: PropTypes.string,
+    })
+  ).isRequired,
+  data: PropTypes.object.isRequired,
+  onChange: PropTypes.func.isRequired,
 };
 
 export default FieldRenderer;
