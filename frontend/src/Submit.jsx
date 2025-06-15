faca097898fec234749eb9bcaf7cdff6dc6f4ccc
AMAN7040 <amansingh79620@gmail.com>
2025-06-15 12:07:06 +0530

    feat(ui): implement styling using tailwind for Submit button

--- frontend/src/Submit.jsx
+++ frontend/src/Submit.jsx
@@ -2,14 +2,13 @@
 
 export const SubmitButton = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <button type="submit">Submit</button>
+    <div className="fixed bottom-6 left-1/2 transform -translate-x-1/2 z-50">
+      <button
+        type="submit"
+        className="bg-purple-500 text-white font-medium py-2 px-6 rounded-full shadow-lg transition duration-300 cursor-pointer"
+      >
+        Submit
+      </button>
     </div>
   );
 };
