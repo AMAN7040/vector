d46d1574c705ea8ef6b2ee65b6c807a99ae75e7f
AMAN7040 <amansingh79620@gmail.com>
2025-06-15 16:14:32 +0530

    stye(ui): add toast container

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,9 @@
 import { PipelineToolbar } from "./Toolbar";
 import { PipelineUI } from "./Ui";
 import { SubmitButton } from "./Submit";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+
 
 function App() {
   return (
@@ -8,6 +11,13 @@ function App() {
       <PipelineToolbar />
       <PipelineUI />
       <SubmitButton />
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        pauseOnHover
+        draggable
+        theme="dark"
+      />
     </div>
   );
 }
